1a14a9376923f7fd58bf1cfcbf2313ccfcf2b861
dcnielsen <dcnielsen90@gmail.com>
2016-12-01 16:27:19 -0500

    Removed Files

--- MediBalance/TcpClient.cs
+++ MediBalance/TcpClient.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace MediBalance
 {
-    class Program
+    class TcpClient
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
