1e24cb02f3e1070e4e23c4985868c52b9e9285d4
David Nielsen <dcnielsen90@gmail.com>
2017-02-02 23:03:04 -0500

    kinect pipe control progress

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -14,21 +14,33 @@ namespace BackEnd
 {
     class Program
     {
+        // Instantiate Named Pipes
         static NamedPipeServerStream kServer = new NamedPipeServerStream("kinect", PipeDirection.InOut);
         static NamedPipeServerStream bServer = new NamedPipeServerStream("board", PipeDirection.InOut);
 
+        /// <summary>
+        /// Main Program
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             runPrograms();
             server(kServer);
         }
 
+        /// <summary>
+        /// Start all of the programs
+        /// </summary>
         static void runPrograms()
         {
             Process.Start("C:\\Users\\dcnie\\Source\\Repos\\MediBalance\\KinectEnvironment\\bin\\Debug\\KinectEnvironment.exe");
             Process.Start("C:\\Users\\dcnie\\Source\\Repos\\MediBalance\\WiiBalanceWalker\\bin\\Debug\\WiiBalanceWalker.exe");
         }
 
+        /// <summary>
+        /// server: takes a stream and makes it a listening server
+        /// </summary>
+        /// <param name="xStream"></param>
         static void server(NamedPipeServerStream xStream)
         {
             // Setup Objects
@@ -43,18 +55,8 @@ namespace BackEnd
             var write = new StreamWriter(xStream) { AutoFlush = true };
             var read = new StreamReader(xStream);
 
-
-            while (message != "exit")
-            {
-                // Poll for response
-                string temp = read.ReadLine();
-                if (temp != null)
-                {
-                    message = temp;
-                    Console.WriteLine(message); // Once there is a line to read, write it to console
-                }
-                Thread.Sleep(500);
-            }
+            message = Console.ReadLine();
+            write.WriteLine(message);
 
             // Close Out Program
             Console.WriteLine("Press any key to exit");
--- KinectEnvironment/Program.cs
+++ KinectEnvironment/Program.cs
@@ -13,6 +13,7 @@ namespace KinectEnvironment
 {
     class Program
     {
+        static NamedPipeClientStream kClient = new NamedPipeClientStream(".", "kinect", PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.Impersonation);
         KinectSensor kinectSensor = KinectSensor.GetDefault();
         BodyFrameReader bodyframeReader = null;
         Body[] bodies = null;
@@ -24,24 +25,64 @@ namespace KinectEnvironment
         [STAThread]
         static void Main()
         {
+            // Declare Variables
+            var command = "";
+            bool run = true;
+
+            // Connect to Pipe
+            kClient.Connect();
+
+            // Run until told to exit
+            while (run) {
+
+                // Listen for command
+                command = listen();
+                Console.WriteLine(command);
+
+                // Perform Command
+                switch (command)
+                {
+                    case "start":
+                        start();
+                        break;
+                    case "test":
+                        test();
+                        break;
+                    case "stop":
+                        run = false;
+                        break;
+                    default:
+                        run = true;
+                        break;
+                }
+            }
+
+            // Close Pipe Connection -- Terminate program
+            kClient.Close();
+        }
+        static string listen()
+        {
+            var read = new StreamReader(kClient);
+            var message = "";
+
+            while (true)
+            {
+                message = read.ReadLine();
+                if (message != null)
+                {
+                    return message;
+                }
+                Thread.Sleep(500);
+            }
+        }
+
+        static void start() { }
+
+        static void test() {
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new KinectForm());
-
-            var pipeClient = new NamedPipeClientStream(".", "kinect", PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.Impersonation);
-            pipeClient.Connect();
-            var write = new StreamWriter(pipeClient) { AutoFlush = true };
-            var read = new StreamReader(pipeClient);
-            var message = "";
-
-            while (message != "exit")
-            {
-                message = Console.ReadLine();
-                write.WriteLine(message);
-            }
-
-
-            pipeClient.Close();
         }
     }
+
 }
