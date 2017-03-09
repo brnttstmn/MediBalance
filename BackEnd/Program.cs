3c29d018f4ad05dab1088a65dbc190c349a1abc2
David Nielsen <dcnielsen90@gmail.com>
2017-03-09 14:52:34 -0500

    Implemented new pipe class and added error handling

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -8,6 +8,7 @@ using System.IO.Pipes;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Linq;
 
 namespace BackEnd
 {
@@ -15,14 +16,15 @@ namespace BackEnd
     {
         // Instantiate Named Pipes
         static Pipe kinect = new Pipe("kinect", new NamedPipeClientStream(".", "kinect", PipeDirection.InOut), "C:\\Users\\" + Environment.UserName + "\\Source\\Repos\\MediBalance\\KinectEnvironment\\bin\\Debug\\KinectEnvironment.exe");
-        static Pipe board = new Pipe("board",new NamedPipeClientStream(".", "board", PipeDirection.InOut), "C:\\Users\\" + Environment.UserName + "\\Source\\Repos\\MediBalance\\BalanceBoard\\bin\\Debug\\BalanceBoard.exe");
+        static Pipe board = new Pipe("board", new NamedPipeClientStream(".", "board", PipeDirection.InOut), "C:\\Users\\" + Environment.UserName + "\\Source\\Repos\\MediBalance\\BalanceBoard\\bin\\Debug\\BalanceBoard.exe");
         static Pipe gui = new Pipe("gui", new NamedPipeServerStream("interface", PipeDirection.InOut), "C:\\Users\\" + Environment.UserName + "\\Source\\Repos\\MediBalance\\FrontEndUIRedux\\bin\\Debug\\FrontEndUIRedux.exe");
-        static Pipe tunnel = new Pipe("tunnel",new NamedPipeClientStream(".", "tunnel", PipeDirection.InOut), "C:\\Users\\" + Environment.UserName + "\\Source\\Repos\\MediBalance\\Tunnel\\bin\\Debug\\Tunnel.exe");
-        static List<Pipe> sensors = new List<Pipe>() { board }; //kinect, board, tunnel
-        static List<Pipe> everything = new List<Pipe>() { board, gui }; //kinect, board, tunnel, gui
+        static Pipe tunnel = new Pipe("tunnel", new NamedPipeClientStream(".", "tunnel", PipeDirection.InOut), "C:\\Users\\" + Environment.UserName + "\\Source\\Repos\\MediBalance\\Tunnel\\bin\\Debug\\Tunnel.exe");
 
+        // Lists
+        static List<Pipe> pipelist = new List<Pipe>() { board, gui }; //kinect, board, tunnel, gui
+        static List<Pipe> sensors = pipelist.Except(new List<Pipe>() { gui }).ToList();
         static List<string> data_list = new List<String>();
-        static string data;
+        
 
         /// <summary>
         /// Main Program
@@ -30,27 +32,37 @@ namespace BackEnd
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-
-            // Start Devices
-            runPrograms();
-
-            // Connect Pipes
-            connectPipes();
-            startSensors();
-            while (true) { readSensor(); }
-                    
+            run();
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
-        static void tempdemocsv()
+
+        static void run()
+        {
+            bool run = true;
+            runPrograms();
+            while (run)
+            {
+                try
+                {
+                    connectPipes();
+                    startSensors();
+                    readSensors();
+                }
+                catch (IOException) { Console.WriteLine("Connection Terminated"); }
+                catch (Exception ex) { Console.WriteLine(ex.ToString()); run = false; }
+                finally { disconnectPipes(); }
+            }
+        }
+
+        static void tempdemocsv(string data)
         {
             bool status = true;
             int i = 0;
             int count = 0;
             while (status == true)
             {
-                //Thread.Sleep(1000);
                 if (!kinect.read.EndOfStream && !string.IsNullOrWhiteSpace(kinect.read.Peek().ToString())) { data = kinect.read.ReadLine(); data_list.Add(data); Console.WriteLine(data); }
                 if (!board.read.EndOfStream && !string.IsNullOrWhiteSpace(board.read.Peek().ToString())) { data = board.read.ReadLine(); data_list.Add(data); Console.WriteLine(data); }
                 if (count > 50)
@@ -70,27 +82,38 @@ namespace BackEnd
             File.AppendAllLines(filePath, data_list);
         }
 
-        static void readSensor()
+        static void readSensors()
         {
-            foreach (Pipe sensor in sensors)
-            {
-                if (!sensor.read.EndOfStream && !string.IsNullOrWhiteSpace(sensor.read.Peek().ToString()))
+                string line;
+                while (true)
                 {
-                    data = sensor.read.ReadLine();
-                    gui.write.Write(data);
-                    data_list.Add(data);
-                    Console.WriteLine(data);
+                    foreach (Pipe sensor in sensors)
+                    {
+                        if (!sensor.read.EndOfStream && !string.IsNullOrWhiteSpace(sensor.read.Peek().ToString()))
+                        {
+                            line = sensor.read.ReadLine();
+                            if (pipelist.Contains(gui)) { gui.write.WriteLine(line); }
+                            data_list.Add(line);
+                            Console.WriteLine(line);
+                        }
+                    }
                 }
-            }
         }
 
         static void connectPipes()
         {
-            foreach (Pipe sensor in sensors)
+            foreach (Pipe pipe in pipelist)
             {
-                sensor.start_client();
+                pipe.start(pipe.pipe);
             }
-            gui.start_server();
+        }
+
+        static void disconnectPipes()
+        {
+            foreach (Pipe pipe in pipelist)
+            {
+                pipe.stop(pipe.pipe);
+            }
         }
 
         static void startSensors()
@@ -106,7 +129,7 @@ namespace BackEnd
         /// </summary>
         static void runPrograms()
         {
-            foreach (Pipe program in everything)
+            foreach (Pipe program in pipelist)
             {
                 Process.Start(program.path);
             }
