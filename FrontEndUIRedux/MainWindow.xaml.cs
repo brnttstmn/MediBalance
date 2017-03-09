b189b16b0f9ebd187dd4a06187611040537cf9aa
David Nielsen <dcnielsen90@gmail.com>
2017-03-08 22:31:53 -0500

    Parse String Data in GUI #9 - Fixed

--- FrontEndUIRedux/MainWindow.xaml.cs
+++ FrontEndUIRedux/MainWindow.xaml.cs
@@ -9,257 +9,24 @@ using System.Threading.Tasks;
 using System.IO.Pipes;
 using System.Diagnostics;
 using System.IO;
+using System.Timers;
 
 namespace FrontEndUIRedux
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        NamedPipeClientStream guiClient = new NamedPipeClientStream(".", "interface", PipeDirection.InOut);
-        StreamWriter write;
-        StreamReader read;
+        NamedPipeClientStream guiClient;
+        StreamReader read;       
 
-        /// <summary>
-        /// Radius of drawn hand circles
-        /// </summary>
-        private const double HandSize = 30;
-
-        /// <summary>
-        /// Thickness of drawn joint lines
-        /// </summary>
-        private const double JointThickness = 3;
-
-        /// <summary>
-        /// Thickness of clip edge rectangles
-        /// </summary>
-        private const double ClipBoundsThickness = 10;
-
-        /// <summary>
-        /// Constant for clamping Z values of camera space points from being negative
-        /// </summary>
-        private const float InferredZPositionClamp = 0.1f;
-
-        /// <summary>
-        /// Brush used for drawing hands that are currently tracked as closed
-        /// </summary>
-        private readonly Brush handClosedBrush = new SolidColorBrush(Color.FromArgb(128, 255, 0, 0));
-
-        /// <summary>
-        /// Brush used for drawing hands that are currently tracked as opened
-        /// </summary>
-        private readonly Brush handOpenBrush = new SolidColorBrush(Color.FromArgb(128, 0, 255, 0));
-
-        /// <summary>
-        /// Brush used for drawing hands that are currently tracked as in lasso (pointer) position
-        /// </summary>
-        private readonly Brush handLassoBrush = new SolidColorBrush(Color.FromArgb(128, 0, 0, 255));
-
-        /// <summary>
-        /// Brush used for drawing joints that are currently tracked
-        /// </summary>
-        private readonly Brush trackedJointBrush = new SolidColorBrush(Color.FromArgb(255, 68, 192, 68));
-
-        /// <summary>
-        /// Brush used for drawing joints that are currently inferred
-        /// </summary>        
-        private readonly Brush inferredJointBrush = Brushes.Yellow;
-
-        /// <summary>
-        /// Pen used for drawing bones that are currently inferred
-        /// </summary>        
-        private readonly Pen inferredBonePen = new Pen(Brushes.Gray, 1);
-
-        /// <summary>
-        /// Drawing group for body rendering output
-        /// </summary>
-        private DrawingGroup drawingGroup;
-
-        /// <summary>
-        /// Drawing image that we will display
-        /// </summary>
-        private DrawingImage imageSource;
-
-        /// <summary>
-        /// Active Kinect sensor
-        /// </summary>
-        private KinectSensor kinectSensor = null;
-
-        /// <summary>
-        /// Coordinate mapper to map one type of point to another
-        /// </summary>
-        private CoordinateMapper coordinateMapper = null;
-
-        /// <summary>
-        /// Reader for body frames
-        /// </summary>
-        private BodyFrameReader bodyFrameReader = null;
-
-        /// <summary>
-        /// Array for the bodies
-        /// </summary>
-        private Body[] bodies = null;
-
-        /// <summary>
-        /// definition of bones
-        /// </summary>
-        private List<Tuple<JointType, JointType>> bones;
-
-        /// <summary>
-        /// Width of display (depth space)
-        /// </summary>
-        private int displayWidth;
-
-        /// <summary>
-        /// Height of display (depth space)
-        /// </summary>
-        private int displayHeight;
-
-        /// <summary>
-        /// List of colors for each body tracked
-        /// </summary>
-        private List<Pen> bodyColors;
-
-        /// <summary>
-        /// Current status text to display
-        /// </summary>
-        private string statusText = null;
+        System.Timers.Timer infoUpdateTimer = new System.Timers.Timer() { Interval = 5, Enabled = false };
 
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
         /// </summary>
         public MainWindow()
         {
-            // one sensor is currently supported
-            this.kinectSensor = KinectSensor.GetDefault();
-
-            // get the coordinate mapper
-            this.coordinateMapper = this.kinectSensor.CoordinateMapper;
-
-            // get the depth (display) extents
-            FrameDescription frameDescription = this.kinectSensor.DepthFrameSource.FrameDescription;
-
-               // get size of joint space
-            this.displayWidth = frameDescription.Width;
-            this.displayHeight = frameDescription.Height;
-
-            // open the reader for the body frames
-            this.bodyFrameReader = this.kinectSensor.BodyFrameSource.OpenReader();
-
-            // a bone defined as a line between two joints
-            this.bones = new List<Tuple<JointType, JointType>>();
-            
-
-            // Torso
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.Head, JointType.Neck));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.Neck, JointType.SpineShoulder));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.SpineMid));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineMid, JointType.SpineBase));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.ShoulderRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineShoulder, JointType.ShoulderLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineBase, JointType.HipRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.SpineBase, JointType.HipLeft));
-
-            // Right Arm
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.ShoulderRight, JointType.ElbowRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.ElbowRight, JointType.WristRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristRight, JointType.HandRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.HandRight, JointType.HandTipRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristRight, JointType.ThumbRight));
-
-            // Left Arm
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.ShoulderLeft, JointType.ElbowLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.ElbowLeft, JointType.WristLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristLeft, JointType.HandLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.HandLeft, JointType.HandTipLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.WristLeft, JointType.ThumbLeft));
-
-            // Right Leg
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.HipRight, JointType.KneeRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.KneeRight, JointType.AnkleRight));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.AnkleRight, JointType.FootRight));
-
-            // Left Leg
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.HipLeft, JointType.KneeLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.KneeLeft, JointType.AnkleLeft));
-            this.bones.Add(new Tuple<JointType, JointType>(JointType.AnkleLeft, JointType.FootLeft));
-
-            // populate body colors, one for each BodyIndex
-            this.bodyColors = new List<Pen>();
-
-            this.bodyColors.Add(new Pen(Brushes.Red, 6));
-            this.bodyColors.Add(new Pen(Brushes.Orange, 6));
-            this.bodyColors.Add(new Pen(Brushes.Green, 6));
-            this.bodyColors.Add(new Pen(Brushes.Blue, 6));
-            this.bodyColors.Add(new Pen(Brushes.Indigo, 6));
-            this.bodyColors.Add(new Pen(Brushes.Violet, 6));
-
-            // set IsAvailableChanged event notifier
-            this.kinectSensor.IsAvailableChanged += this.Sensor_IsAvailableChanged;
-
-            // open the sensor
-            this.kinectSensor.Open();
-
-            // set the status text
-            this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
-                                                            : Properties.Resources.NoSensorStatusText;
-
-            // Create the drawing group we'll use for drawing
-            this.drawingGroup = new DrawingGroup();
-
-            // Create an image source that we can use in our image control
-            this.imageSource = new DrawingImage(this.drawingGroup);
-
-            // use the window object as the view model in this simple example
-            this.DataContext = this;
-
-            // initialize the components (controls) of the window
             this.InitializeComponent();
         }
-
-        /// <summary>
-        /// INotifyPropertyChangedPropertyChanged event to allow window controls to bind to changeable data
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Gets the bitmap to display
-        /// </summary>
-        public ImageSource ImageSource
-        {
-            get
-            {
-                return this.imageSource;
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the current status text to display
-        /// </summary>
-        public string StatusText
-        {
-            get
-            {
-                return this.statusText;
-            }
-
-            set
-            {
-                if (this.statusText != value)
-                {
-                    this.statusText = value;
-
-                    // notify any bound elements that the text has changed
-                    if (this.PropertyChanged != null)
-                    {
-                        this.PropertyChanged(this, new PropertyChangedEventArgs("StatusText"));
-                    }
-                }
-            }
-        }
-
-        public EventHandler<IsAvailableChangedEventArgs> Sensor_IsAvailableChanged { get; private set; }
 
         /// <summary>
         /// Execute start up tasks
@@ -268,6 +35,7 @@ namespace FrontEndUIRedux
         /// <param name="e">event arguments</param>
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            infoUpdateTimer.Elapsed += new ElapsedEventHandler(infoUpdateTimer_Elapsed);
         }
 
         /// <summary>
@@ -277,18 +45,6 @@ namespace FrontEndUIRedux
         /// <param name="e">event arguments</param>
         private void MainWindow_Closing(object sender, CancelEventArgs e)
         {
-            if (this.bodyFrameReader != null)
-            {
-                // BodyFrameReader is IDisposable
-                this.bodyFrameReader.Dispose();
-                this.bodyFrameReader = null;
-            }
-
-            if (this.kinectSensor != null)
-            {
-                this.kinectSensor.Close();
-                this.kinectSensor = null;
-            }
         }
 
         
@@ -318,21 +74,20 @@ namespace FrontEndUIRedux
             StartButton.IsEnabled = false;
             Task.Run(() =>
             {
+                guiClient = new NamedPipeClientStream(".", "interface", PipeDirection.InOut);
                 guiClient.Connect();
                 read = new StreamReader(guiClient);
-                Stopwatch s = new Stopwatch();
-                s.Start();
-                while (s.Elapsed < TimeSpan.FromSeconds(10))
-                {
-                    Console.WriteLine(read.ReadLine());
-                }
+                infoUpdateTimer.Enabled = true;
+                System.Threading.Thread.Sleep(10000);
+                guiClient.Dispose();
+                infoUpdateTimer.Enabled = false;
                 this.Dispatcher.Invoke(() =>
                 {
                     StartButton.IsEnabled = true;
                 });                
             });
-            
         }
+
         private void ExportButton_Click(object sender, RoutedEventArgs e)
         {
 
@@ -341,12 +96,136 @@ namespace FrontEndUIRedux
         {
 
         }
-
         private void MenuItem_board_Click_1(object sender, RoutedEventArgs e)
         {
             BoardWindow popup = new BoardWindow();
             popup.ShowDialog();
         }
+
+        void infoUpdateTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var line = read.ReadLine();
+            this.Dispatcher.Invoke(() =>
+            {
+                if (line != null) { parse(line); }                
+            });
+        }
+
+        private void parse(string line)
+        {
+            char[] delim = { ',', ';' };
+            string[] words = line.Split(delim);
+                if (words.Length > 2)
+                {
+                    switch (words[1])
+                    {
+                        case "RWeight":
+                            RWeight.Content = words[2];
+                            break;
+                        case "TopLeft":
+                            TLeft.Content = words[2];
+                            break;
+                        case "TopRight":
+                            TRight.Content = words[2];
+                            break;
+                        case "BottomRight":
+                            BRight.Content = words[2];
+                            break;
+                        case "BottomLeft":
+                            BLeft.Content = words[2];
+                            break;
+                        case "spinebase":
+                            SpineBaseX.Text = words[2];
+                            SpineBaseY.Text = words[3];
+                            SpineBaseZ.Text = words[4];
+                        break;
+                        case "midspine":
+                            MidSpineX.Text = words[2];
+                            MidSpineY.Text = words[3];
+                            MidSpineZ.Text = words[4];
+                            break;
+                        case "neck":
+                            NeckX.Text = words[2];
+                            NeckY.Text = words[3];
+                            NeckZ.Text = words[4];
+                            break;
+                        case "shoulderleft":
+                            ShoulderLeftX.Text = words[2];
+                            ShoulderLeftY.Text = words[3];
+                            ShoulderLeftZ.Text = words[4];
+                            break;
+                        case "elbowleft":
+                            ElbowLeftX.Text = words[2];
+                            ElbowLeftY.Text = words[3];
+                            ElbowLeftZ.Text = words[4];
+                            break;
+                        case "wristleft":
+                            WristLeftX.Text = words[2];
+                            WristLeftY.Text = words[3];
+                            WristLeftZ.Text = words[4];
+                            break;
+                        case "handleft":
+                            HandLeftX.Text = words[2];
+                            HandLeftY.Text = words[3];
+                            HandLeftZ.Text = words[4];
+                            break;
+                        case "head":
+                            HeadX.Text = words[2];
+                            HeadY.Text = words[3];
+                            HeadZ.Text = words[4];
+                            break;
+                    case "shoulderright":
+                        ShoulderRightX.Text = words[2];
+                        ShoulderRightY.Text = words[3];
+                        ShoulderRightZ.Text = words[4];
+                        break;
+                // Brent you didn't finish putting in all of the fields!!!!
+                    //case "elbowright":
+                    //    elbo.Text = words[2];
+                    //    MidSpineY.Text = words[3];
+                    //    MidSpineZ.Text = words[4];
+                    //    break;
+                    //case "handright":
+                    //    NeckX.Text = words[2];
+                    //    NeckY.Text = words[3];
+                    //    NeckZ.Text = words[4];
+                    //    break;
+                    //case "hipleft":
+                    //    SpineBaseX.Text = words[2];
+                    //    SpineBaseY.Text = words[3];
+                    //    SpineBaseZ.Text = words[4];
+                    //    break;
+                    //case "kneeleft":
+                    //    SpineBaseX.Text = words[2];
+                    //    SpineBaseY.Text = words[3];
+                    //    SpineBaseZ.Text = words[4];
+                    //    break;
+                    //case "footleft":
+                    //    MidSpineX.Text = words[2];
+                    //    MidSpineY.Text = words[3];
+                    //    MidSpineZ.Text = words[4];
+                    //    break;
+                    //case "hipright":
+                    //    NeckX.Text = words[2];
+                    //    NeckY.Text = words[3];
+                    //    NeckZ.Text = words[4];
+                    //    break;
+                    //case "kneeright":
+                    //    HeadX.Text = words[2];
+                    //    HeadY.Text = words[3];
+                    //    HeadZ.Text = words[4];
+                    //    break;
+                    //case "footright":
+                    //    HeadX.Text = words[2];
+                    //    HeadY.Text = words[3];
+                    //    HeadZ.Text = words[4];
+                    //    break;
+                    default:
+                            break;
+                    }
+                }
+            }
+        }
+
     }
-}
 
