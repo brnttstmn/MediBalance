0dd45bde81fe94f86fb277720125ea8820c2570e
David Nielsen <dcnielsen90@gmail.com>
2017-04-11 11:05:28 -0400

    Added save block; still needs implementation

---
+++ FrontEndUIRedux/Save.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontEndUIRedux
+{
+    class Save
+    {
+        public string chooseLocation()
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".csv"; // Default file extension
+            dlg.Filter = "CSV (.csv)|*.csv"; // Filter files by extension
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                return dlg.FileName;
+            }
+
+            return null;
+        }
+    }
+}
